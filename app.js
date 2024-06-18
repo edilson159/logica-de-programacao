b799a05d22f528535d8fd50fe92d5c595b0b740d
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-06-18 14:07:35 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 alert("Boas vindas ao jogo do número secreto");
-let numberSecret = 29;
+let numberSecret = 10;
 
-let kick = prompt("Escolha um número entre 1 e 30");
+let kick;
 
-if (numberSecret == kick) {
-  alert("Isso ai, Você acertou qual é o número secreto !!!");
-} else {
-  alert("Você errou seu burro");
-}
-
-if (numberSecret < kick) {
-  alert("O número secreto é menor ");
-}
-
-if (numberSecret > kick) {
-  alert("O número secreto é maior ");
+while (kick != numberSecret) {
+  kick = prompt("Escolha um número entre 1 e 30");
+  if (numberSecret == kick) {
+    alert(
+      `Isso ai, Você acertou qual é o número secreto, Você eh fodaaaaaa , O número secreto é ${numberSecret} !!!`
+    );
+  } else {
+    alert("Você errou seu burro");
+    if (numberSecret < kick) {
+      alert(`O número secreto é menor que o chute ${kick}`);
+    } else {
+      alert(`O número secreto é maior que o chute ${kick}`);
+    }
+  }
 }
